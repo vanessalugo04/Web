1857d06f26ee136f1de6c50f8bb0d998092f6302
Vanessa <vlugoavalos@gmail.com>
2026-04-27 19:23:54 -0600

    php

---
+++ PHP/pagina.php
@@ -0,0 +1,20 @@
+/*
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Mi primera pagina con php</h1>
+
+<?php
+echo 'ola de saludo, no de mar ';
+//esto es un comentario de una linea
+# esto tambien es un comentario
+
+/* esto es un comentario de varias lineas
+que se extiende por varias lineas
+*/  
+?>
+
+</body>
+</html>
---
+++ PHP/variables.php
@@ -0,0 +1,33 @@
+/* las variables contienen datos para
+que puedan ser usados. Debe comenzar por $ seguido de una letra o guin bajo,
+no puede comenzar con un numero
+*/
+<?php
+$x  = 22;
+$y = "hola";
+
+echo "El valor de x es: $x";
+
+/* No hace falta especificar el tipo de dato. Los tipos de datos son
+String
+Int
+Float
+Boolean
+Array
+Object
+Null
+Resource: Hace referencia a recursos externos
+Mixed: Cualquier valor
+*/
+
+var_dump(5);
+var_dump("John");
+var_dump(3.14);
+var_dump(true);
+var_dump([2, 3, 56]);
+var_dump(NULL);
+
+//asignacion multiple
+$a = $b = $c = 10; 
+
+?>
